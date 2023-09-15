b6b5335c29959d3465665208d64132486041cf26
aliceric27 <aliceric27@gmail.com>
2023-09-15 18:04:20 +0800

    init upload dcm

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,103 @@
+"use client";
 import Image from 'next/image'
+import Button from '@mui/material/Button';
+import { useState,useEffect,useRef } from 'react';
+import * as dicomParser from 'dicom-parser';
 
 export default function Home() {
+  const [patientName, setPatientName] = useState<string | null>(null);
+  const [birthDate, setBirthDate] = useState<string | null>(null);
+  const [patientAge, setPatientAge] = useState<string | null>(null);
+  const [patientSex, setPatientSex] = useState<string | null>(null);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  function parseDicom(arrayBuffer: any) {
+
+    const byteArray = new Uint8Array(arrayBuffer);
+    const dataSet:any = dicomParser.parseDicom(byteArray);
+    setPatientName(dataSet.string('x00100010'));
+    setBirthDate(dataSet.string('x00100030'));
+    setPatientAge(dataSet.string('x00101010'));  // DICOM標籤為病患年齡
+    setPatientSex(dataSet.string('x00100040'));
+    const width = dataSet.uint16('x00280010'); // Rows
+    const height = dataSet.uint16('x00280011'); // Columns
+    const bitsAllocated = dataSet.uint16('x00280100'); // Bits Allocated (e.g., 8, 16)
+    // 從dataSet提取像素數據
+    const pixelDataElement = dataSet.elements.x7fe00010;
+    const pixelData = new Uint8Array(dataSet.byteArray.buffer, pixelDataElement.dataOffset, pixelDataElement.length);
+
+    // 使用canvas來轉換像素數據為Base64圖像URL
+   const canvas:{} = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    const context = canvas.getContext('2d');
+    const imageData = context.createImageData(width, height);
+    if (bitsAllocated === 8) {
+      for (let i = 0; i < pixelData.length; i++) {
+          imageData.data[4 * i] = pixelData[i];      // R
+          imageData.data[4 * i + 1] = pixelData[i];  // G
+          imageData.data[4 * i + 2] = pixelData[i];  // B
+          imageData.data[4 * i + 3] = 255;           // A
+      }
+  }
+  context.putImageData(imageData, 0, 0);
+  const base64Image = canvas.toDataURL();
+  setSelectedImage(base64Image);
+  }
+
+  
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+        reader.onload = function(event:any) {
+            parseDicom(event.target.result);
+        }
+        reader.readAsArrayBuffer(file);
+    }
+    }
+  
+    const canvasRef = useRef(null);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className='grid grid-cols-4 h-screen'>
+        <div className='bg-red-500 h-full col-span-3 p-10'>
+          <input 
+            type="file" 
+            accept=".dcm" 
+            onChange={handleImageChange} 
+            style={{ display: 'none' }} 
+            id="upload-input"
+          />
+          <label htmlFor="upload-input">
+            <Button component="span" className="my-3" variant="contained">
+              Upload
+            </Button>
+          </label>
+          <div>
+            <p>Patient Name:{patientName}</p>
+            <p>Birthday:{birthDate}</p>
+            <p>Age:{patientAge}</p>
+            <p>Sex:{patientSex}</p>
+            <div style={{position: 'relative'}}>
+            <div className='my-5 bg-slate-500 max-h-[400px] w-full'>
+            {selectedImage && <Image width={400} height={400} src={selectedImage} alt="Uploaded Preview" objectFit="contain" />}
+            </div>
+            </div>
+          </div>
+        </div>
+        <div className='bg-blue-500 h-full p-10'>
+          <p>Label Tools</p>
+          <Button className="my-3" variant="contained">Add</Button>
+          <div>
+            <p>Label List</p>
+            <ul>
+              <li>label1 </li>
+            </ul>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   )
-}
+  }
